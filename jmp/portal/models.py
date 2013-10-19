cf3b49e8be0b52ca4391871b69cd5dbd5243b882
Nathan McCourtney <nathan@mccourtney.com>
2013-10-19 17:52:04 -0400

    Basic admin site running

--- jmp/portal/models.py
+++ jmp/portal/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Proxy(models.Model):
+    proxy_name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.proxy_name
+
+class PConfig(models.Model):
+    proxy = models.ForeignKey(Proxy)
+    pconfig_name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.pconfig_name
+
+class ContentSwitch(models.Model):
+    pconfig = models.ForeignKey(PConfig)
+    content_switch_name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.content_switch_name
+
+class Target(models.Model):
+    content_switch = models.ForeignKey(ContentSwitch)
+    target_name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.target_name
+
+class Endpoint(models.Model):
+    target = models.ForeignKey(Target)
+    endpoint_name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.endpoint_name
